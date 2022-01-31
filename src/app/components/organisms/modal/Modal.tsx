e32721274dec565ec28f405d9a0d559383686d29
emmanelos <emmanelosg@gmail.com>
2022-01-31 05:31:31 -0400

    Init form component

--- src/app/components/organisms/modal/Modal.tsx
+++ src/app/components/organisms/modal/Modal.tsx
@@ -1,8 +1,9 @@
+import Form from '../form/Form';
+
 const Modal = () => {
 	return (
 		<section className="modal">
-			<p className="modal__title">Thinking with portals</p>
-			<button>Hola</button>
+			<Form />
 		</section>
 	);
 };
